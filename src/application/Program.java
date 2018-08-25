4400251786c65e3d24e72a235a714b770f21d89f
Franklin N. Silva <franklin.tekk@hotmail.com>
2018-08-25 12:49:23 -0300

    Created JavaFX Stage and Scene

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,27 @@
 package application;
 
-public class Program {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Program extends Application{
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello World");
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		Button btn = new Button("Click me");
+		
+		StackPane root = new StackPane();
+		root.getChildren().add(btn);
+		Scene scene = new Scene(root, 400, 300);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		
 	}
 
 }
