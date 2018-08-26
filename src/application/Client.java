831fb9a0046eef7d8003b06712becce67e02c2ab
Franklin N. Silva <franklin.tekk@hotmail.com>
2018-08-26 19:01:51 -0300

    Adicionado funcionalidade botão Home

--- src/application/Client.java
+++ src/application/Client.java
@@ -23,7 +23,7 @@ public class Client extends Application {
 
 	}
 
-	public Stage getStage() {
+	public static Stage getStage() {
 		return stage;
 	}
 
--- src/controller/ClientController.java
+++ src/controller/ClientController.java
@@ -3,36 +3,53 @@ package controller;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-import javafx.fxml.FXML;
+import application.Client;
+import application.Home;
 import javafx.fxml.Initializable;
+import javafx.fxml.FXML;
+
 import javafx.scene.control.Button;
 import javafx.scene.control.MenuButton;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
 
 public class ClientController implements Initializable {
 
-	@FXML
-	private MenuButton btnBoleto;
+    @FXML
+    private MenuButton btnBoleto;
 
-	@FXML
-	private MenuButton btnRelatorio;
+    @FXML
+    private MenuButton btnRelatorio;
 
-	@FXML
-	private TableView<?> tbCliente;
+    @FXML
 
-	@FXML
-	private TableColumn<?, ?> tbColumnID;
+    @FXML
+    private TableColumn<?, ?> tbColumnID;
 
-	@FXML
-	private TableColumn<?, ?> tbColumnName;
+    @FXML
+    private TableColumn<?, ?> tbColumnName;
 
-	@FXML
-	private TableColumn<?, ?> tbColumnCPF;
+    @FXML
+    private TableColumn<?, ?> tbColumnCPF;
 
-	@FXML
-	private Button btnCadastrarCliente;
+    @FXML
+    private Button btnCadastrarCliente;
+
+    @FXML
+    private ImageView imgReturn;
 
 	public void initialize(URL url, ResourceBundle rb) {
+		imgReturn.setOnMouseClicked(MouseEvent -> {
+			Client.getStage().close();
+			Home home = new Home();
+			try {
+				home.start(new Stage());
+			} catch (Exception e) {
+				e.getMessage();
+			}
+
+		});
 	}
 }
