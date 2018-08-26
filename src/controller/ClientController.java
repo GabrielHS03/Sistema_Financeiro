7dd8bc680bc74cb309994b59ac8d6f1009331bd8
Gabriel Henrique <gabriel.hs03@outlook.com>
2018-08-26 17:26:38 -0300

    Alterações na Tela Cliente

--- src/controller/ClientController.java
+++ src/controller/ClientController.java
@@ -1,5 +1,38 @@
 package controller;
 
-public class ClientController {
+import java.net.URL;
+import java.util.ResourceBundle;
 
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class ClientController implements Initializable {
+
+	@FXML
+	private MenuButton btnBoleto;
+
+	@FXML
+	private MenuButton btnRelatorio;
+
+	@FXML
+	private TableView<?> tbCliente;
+
+	@FXML
+	private TableColumn<?, ?> tbColumnID;
+
+	@FXML
+	private TableColumn<?, ?> tbColumnName;
+
+	@FXML
+	private TableColumn<?, ?> tbColumnCPF;
+
+	@FXML
+	private Button btnCadastrarCliente;
+
+	public void initialize(URL url, ResourceBundle rb) {
+	}
 }
