27494eb5f880f82caebcad1f0c3d2d0d5601bf0c
AlysonACM <alyson.antonio@gmail.com>
2018-08-26 14:51:52 -0300

    Atualizando Usuario

---
+++ src/controller/UsuarioController.java
@@ -0,0 +1,32 @@
+package controller;
+
+import javax.persistence.Entity;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import model.Usuario;
+
+public class UsuarioController {
+
+	public static void main(String[] args) {
+
+
+		Usuario newUsuario = new Usuario();
+		newUsuario.setUsuario("Alessandra");
+		
+		newUsuario.setSenha("159159");
+		
+	
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("FinanceiroUnit"); 
+		EntityManager entityManager = factory.createEntityManager();
+		
+		entityManager.getTransaction().begin();
+		entityManager.persist(newUsuario);
+		entityManager.getTransaction().commit();
+		
+		entityManager.close();
+		factory.close();
+	}
+
+}
--- src/model/Usuario.java
+++ src/model/Usuario.java
@@ -1,5 +1,50 @@
 package model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+//Entity e para especificar que vai ser uma tabela no banco , que no banco e TB_Usuario!
+
+@Entity(name = "TB_Usuario")
+
 public class Usuario {
+
+	// ID para especificar para o banco de dados que e um ID
+	// Generated e para especificar para o banco que e uma chave primaria de auto Incremental
+	
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer ID;
+	private String usuario;
+	private String senha;
+	
+	
+
+	public Integer getID() {
+		return ID;
+	}
+
+	public void setID(Integer iD) {
+		ID = iD;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
 
 }
