ff99157e0e1992c37c0979b194147a529bc36964
AlysonACM <alyson.antonio@gmail.com>
2018-08-26 14:55:57 -0300

    New Teste Usuario

--- src/controller/UsuarioController.java
+++ src/controller/UsuarioController.java
@@ -1,6 +1,5 @@
 package controller;
 
-import javax.persistence.Entity;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
