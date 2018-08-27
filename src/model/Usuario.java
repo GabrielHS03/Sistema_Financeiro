812cf5c3508c7d2eb06ec9191edfd0f341d02e40
AlysonACM <alyson.antonio@gmail.com>
2018-08-27 10:25:48 -0300

    Refazendo

--- src/model/Usuario.java
+++ src/model/Usuario.java
@@ -1,50 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
-//Entity e para especificar que vai ser uma tabela no banco , que no banco e TB_Usuario!
-
-@Entity(name = "TB_Usuario")
-
+/**
+ *
+ * @author Alyson-Casa
+ */
 public class Usuario {
-
-	// ID para especificar para o banco de dados que e um ID
-	// Generated e para especificar para o banco que e uma chave primaria de auto Incremental
-	
-
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer ID;
-	private String usuario;
-	private String senha;
-	
-	
-
-	public Integer getID() {
-		return ID;
-	}
-
-	public void setID(Integer iD) {
-		ID = iD;
-	}
-
-	public String getUsuario() {
-		return usuario;
-	}
-
-	public void setUsuario(String usuario) {
-		this.usuario = usuario;
-	}
-
-	public String getSenha() {
-		return senha;
-	}
-
-	public void setSenha(String senha) {
-		this.senha = senha;
-	}
-
+    
 }
