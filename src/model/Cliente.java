ee9a12292782ac58149b7916e33012b2629f6195
AlysonACM <alyson.antonio@gmail.com>
2018-08-27 12:26:07 -0300

    Class Cliente

--- src/model/Cliente.java
+++ src/model/Cliente.java
@@ -5,10 +5,103 @@
  */
 package model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /**
  *
  * @author Alyson-Casa
  */
+
+@Entity(name = "TB_Cliente")
 public class Cliente {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer ID;
+    private Integer codigo;
+    private String nome;
+    private String telefone;
+    private String tipo;
+    private Double CPF;
+    private Double CNPJ;
+    private String email;
+    private String OBS;
+
+    public Integer getID() {
+        return ID;
+    }
+
+    public void setID(Integer ID) {
+        this.ID = ID;
+    }
+
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Integer codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public Double getCPF() {
+        return CPF;
+    }
+
+    public void setCPF(Double CPF) {
+        this.CPF = CPF;
+    }
+
+    public Double getCNPJ() {
+        return CNPJ;
+    }
+
+    public void setCNPJ(Double CNPJ) {
+        this.CNPJ = CNPJ;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getOBS() {
+        return OBS;
+    }
+
+    public void setOBS(String OBS) {
+        this.OBS = OBS;
+    }
+    
+    
     
 }
