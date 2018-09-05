15383c881e67af212d45451a7b995069f79a59ed
Gabriel Henrique <gabriel.hs03@outlook.com>
2018-09-05 19:07:15 -0300

    Alteração da tela de alteração

--- src/controller/ClienteAlterarController.java
+++ src/controller/ClienteAlterarController.java
@@ -6,10 +6,6 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import DAO.ClienteDAO;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.collections.transformation.FilteredList;
-import javafx.collections.transformation.SortedList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -20,9 +16,7 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
 import model.Cliente;
 
@@ -112,20 +106,11 @@ public class ClienteAlterarController implements Initializable {
 	@FXML
 	private ImageView imgHome;
 	
-	private ObservableList<Cliente> listaClientes = FXCollections.observableArrayList();;
-
 	
 	public void initialize(URL url, ResourceBundle rb) {
 		
 		setarDados();
-		comboBox.getItems().addAll("CPF", "CNPJ");
-		choice();
 		
-		carregarTableViewClientes();
-		
-		tbCliente.getSelectionModel().selectedItemProperty()
-				.addListener((observable, oldValue, newValue) -> carregarDadosCliente(newValue));
-
 		imgHome.setOnMouseClicked(MouseEvent -> {
 			AnchorPane pane;
 			try {
@@ -147,14 +132,8 @@ public class ClienteAlterarController implements Initializable {
 			cliente.setEmail(txtEmail.getText());
 			cliente.setOBS(txtObservacao.getText());
 			
-			if(comboBox.getValue() == "CPF") {
-				cliente.setCPF(Long.parseLong(txtCPFCNPJ.getText()));
-			}else {
-				cliente.setCNPJ(Long.parseLong(txtCPFCNPJ.getText()));
-				cliente.setRazaoSocial(txtRazaoSocial.getText());
-			}
-			
 			clienteDAO.save(cliente);
+			
 			try {
 				recarregarTela();
 			} catch (IOException e) {
@@ -165,85 +144,9 @@ public class ClienteAlterarController implements Initializable {
 		});
 	}
 
-	public void choice() {
-
-		comboBox.setOnAction(e -> {
-			switch (comboBox.getValue()) {
-			case "CPF":
-				txtRazaoSocial.setVisible(false);
-				lblCNPJ.setVisible(false);
-				lblCPFCNPJ.setText("CPF:*");
-				lblNome.setText("Nome:*");
-				break;
-			case "CNPJ":
-				txtRazaoSocial.setVisible(true);
-				lblCNPJ.setVisible(true);
-				lblCPFCNPJ.setText("CNPJ:*");
-				lblNome.setText("Nome Fantasia:*");
-				break;
-			}
-		});
-	}
-
-	public void carregarTableViewClientes() {
-		
-		ClienteDAO clienteDAO = new ClienteDAO();
-		for (Cliente cliente : clienteDAO.buscarTodos()) {
-
-			listaClientes.add(cliente);
-		}
-		
-		tbCliente.setItems(listaClientes);
-		tbClienteColumnID.setCellValueFactory(new PropertyValueFactory<>("codigo"));
-		tbClienteColumnNome.setCellValueFactory(new PropertyValueFactory<>("nome"));
-		tbClienteColumnCPF.setCellValueFactory(new PropertyValueFactory<>("CPF"));
-		tbClienteColumnCNPJ.setCellValueFactory(new PropertyValueFactory<>("CNPJ"));
-		tbClienteColumnTelefone.setCellValueFactory(new PropertyValueFactory<>("telefone"));
-	}
-		
-    @FXML
-    public void pesquisarTabela(KeyEvent event) {
-
-    	FilteredList<Cliente> listaClientesFiltered = new FilteredList<>(listaClientes, p -> true);
-        searchBox.textProperty().addListener((obsevable, oldvalue, newvalue) -> {
-        	listaClientesFiltered.setPredicate(cliente -> {
-
-                if (newvalue == null || newvalue.isEmpty()) {
-                    return true;
-                }
-                String typedText = newvalue.toLowerCase();
-               
-                String codigoEmString = Integer.toString(cliente.getCodigo());
-                String cpfEmString = String.valueOf(cliente.getCPF());
-                String cnpjEmString = String.valueOf(cliente.getCNPJ());
-                
-                if ((cliente.getNome().toLowerCase().indexOf(typedText) != -1) || (codigoEmString.toLowerCase().indexOf(typedText) != -1) || (cpfEmString.toLowerCase().indexOf(typedText) != -1) || (cnpjEmString.toLowerCase().indexOf(typedText) != -1)) {
-
-                    return true;
-                }
-
-                
-                return false;
-            });
-            SortedList<Cliente> listaClientesSorted = new SortedList<>(listaClientesFiltered);
-            listaClientesSorted.comparatorProperty().bind(tbCliente.comparatorProperty());
-            tbCliente.setItems(listaClientesSorted);                      
-        });
-    }
-
 	public void recarregarTela() throws IOException {
 		AnchorPane pane = FXMLLoader.load(getClass().getResource("/view/Cliente.Principal.fxml"));
 		rootPane.getChildren().setAll(pane);
-	}
-	
-	public void carregarTelaAlterar() throws IOException {
-		AnchorPane pane = FXMLLoader.load(getClass().getResource("/view/Cliente.Alterar.fxml"));
-		rootPane.getChildren().setAll(pane);
-	}
-	
-	public void carregarDadosCliente(Cliente cliente) {
-		System.out.println(cliente.getNome());
-		
 	}
 	
 	public void setarDados() {
